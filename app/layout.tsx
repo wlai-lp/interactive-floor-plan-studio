0c7d16e2a522f7736b8f5696e8c18e2d33638c5c
Wei Lai <99457898+wlai-lp@users.noreply.github.com>
2026-08-14 09:05:54 -0400

    feat: enable required device field UX

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import DeviceRequiredFieldEnhancer from "./device-required-field-enhancer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <DeviceRequiredFieldEnhancer />
         {children}
       </body>
     </html>
