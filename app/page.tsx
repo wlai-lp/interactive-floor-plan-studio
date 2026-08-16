0d4f3996ee2bbbad12156089e028387d0369554a
Wei Lai <99457898+wlai-lp@users.noreply.github.com>
2026-08-16 08:24:58 -0400

    feat: add landing workflow below hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { LandingHero } from "../components/marketing/LandingHero";
+import { LandingWorkflow } from "../components/marketing/LandingWorkflow";
 import { MarketingShell } from "../components/marketing/MarketingShell";
 
 export default function Page() {
   return (
     <MarketingShell>
       <LandingHero />
+      <LandingWorkflow />
     </MarketingShell>
   );
 }
